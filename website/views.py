13ebf2eece0e72a0d7b31e4f1a845ef021b09386
D1ffamp <marcam82@gmail.com>
2023-05-28 14:18:04 +0000

    created views for table and booking

--- website/views.py
+++ website/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Table, Booking
 
-# Create your views here.
+
+def table_list(request):
+    # Retrieve all tables from the database
+    tables = Table.objects.all()
+
+    # Render the 'table_list.html' template with the tables data
+    return render(request, 'table_list.html', {'tables': tables})
+
+
+def booking_list(request):
+    # Retrieve all bookings from the database
+    bookings = Booking.objects.all()
+
+    # Render the 'booking_list.html' template with the bookings data
+    return render(request, 'booking_list.html', {'bookings': bookings})
