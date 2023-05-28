4ba726270e1c68516e79855daab96c525a9e24e9
D1ffamp <marcam82@gmail.com>
2023-05-28 11:09:56 +0000

    created booking model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Booking(models.Model):
+    """
+    Model to represent a booking made by a user.
+    The meta class orders each booking by reservation date in descending order.
+    str returns the reservation date and time to be used as booking title
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    num_guests = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return f'{self.date} {self.time}'
