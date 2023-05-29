2bfc0645f675220f37393d984171d0457e4774a5
D1ffamp <marcam82@gmail.com>
2023-05-29 13:49:42 +0000

    updated urls index, table and booking

--- tonyspizza/urls.py
+++ tonyspizza/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import IndexView, TableListView, BookingListView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('tables/', TableListView.as_view(), name='table-list'),
+    path('bookings/', BookingListView.as_view(), name='booking-list'),
 ]
