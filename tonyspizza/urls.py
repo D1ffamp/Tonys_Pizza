2ef2225e014533212131194ba86af6b6c65d4cf3
D1ffamp <marcam82@gmail.com>
2023-05-29 11:23:22 +0000

    wired up index.hmtl in views and url.py

--- tonyspizza/urls.py
+++ tonyspizza/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import IndexView, TableListView, BookingListView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from django.shortcuts import render
 from .models import Table, Booking
 
@@ -17,7 +17,11 @@ class BookingListView(ListView):
     model = Booking
     template_name = 'booking_list.html'
     context_object_name = 'bookings'
-    # The 'model' attribute specifies the model to retrieve data from
-    # The 'template_name' attribute specifies the template to render
-    # The 'context_object_name' attribute specifies the name of the
+    # The model attribute specifies the model to retrieve data from
+    # The template_name attribute specifies the template to render
+    # The context_object_name attribute specifies the name of the
     #  context variable in the template
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
