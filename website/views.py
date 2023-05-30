1e9d2a6f6fe34881b804eb9ddfbff06974b047cd
D1ffamp <marcam82@gmail.com>
2023-05-30 11:31:18 +0000

    added login view

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Table, Booking
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class TableListView(ListView):
@@ -17,3 +19,19 @@ class BookingListView(ListView):
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
