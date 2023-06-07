267c3f000eb4cdd80c710ed0390607fc6a4f6b22
D1ffamp <marcam82@gmail.com>
2023-06-07 14:15:54 +0000

    modified create, update and delete views to class based

--- website/views.py
+++ website/views.py
@@ -1,148 +1,145 @@
-from django.views.generic import (
-    ListView,
-    TemplateView,
-    CreateView,
-    UpdateView,
-    DeleteView
-)
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Table, Booking
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.decorators import login_required
-from .forms import BookingForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView, CreateView, UpdateView, DeleteView
+from allauth.account.views import LoginView  # Import the LoginView from allauth
+from .forms import BookingForm
+from .models import Table, Booking
 
 
 class TableListView(ListView):
+    """
+    A view that lists all the tables.
+
+    Inherits from Django's ListView class to provide
+    a list view for the tables.
+    """
+
     model = Table
     template_name = 'table_list.html'
     context_object_name = 'tables'
 
 
-class BookingListView(ListView):
+class BookingListView(LoginRequiredMixin, ListView):
+    """
+    A view that lists the bookings for the authenticated user.
+
+    Inherits from Django's ListView class to provide a list view for the
+    bookings.
+    Requires the user to be logged in.
+
+    Attributes:
+        model (Model): The model to use for the list view (Booking).
+        template_name (str): The name of the template used to render the view.
+        context_object_name (str): The name of the variable to use in the
+        template for the bookings.
+    """
+
     model = Booking
     template_name = 'booking_list.html'
     context_object_name = 'bookings'
 
+    def get_queryset(self):
+        """
+        Returns the queryset of bookings for the authenticated user.
+        """
+        return Booking.objects.filter(user=self.request.user)
+
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
-
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('index')
-    else:
-        form = AuthenticationForm()
-
-    return render(request, 'login.html', {'form': form})
-
-
-@login_required
-def booking_list(request):
-    bookings = Booking.objects.filter(user=request.user)
-    return render(request, 'booking_list.html', {'bookings': bookings})
-
-
-@login_required
-def booking_create(request):
-    if request.method == 'POST':
-        form = BookingForm(request.POST)
-        if form.is_valid():
-            booking = form.save(commit=False)
-            booking.user = request.user
-            booking.save()
-            return redirect('booking-list')
-    else:
-        form = BookingForm()
-    return render(request, 'booking_create.html', {'form': form})
-
-
-@login_required
-def booking_update(request, pk):
-    booking = get_object_or_404(Booking, pk=pk, user=request.user)
-    if request.method == 'POST':
-        form = BookingForm(request.POST, instance=booking)
-        if form.is_valid():
-            form.save()
-            return redirect('booking-list')
-    else:
-        form = BookingForm(instance=booking)
-    return render(request, 'booking_update.html',
-                  {'form': form, 'booking': booking})
-
-
-@login_required
-def booking_delete(request, pk):
-    booking = get_object_or_404(Booking, pk=pk, user=request.user)
-    if request.method == 'POST':
-        booking.delete()
-        return redirect('booking-list')
-    return render(request, 'booking_delete.html', {'booking': booking})
-
-
-class BookingCreateView(CreateView):
     """
-    A view that handles the creation of new bookings.
+    A view that renders the index page.
 
-    Inherits from Django's CreateView class and provides the necessary
-    attributes to customize its behavior.
+    Inherits from Django's TemplateView class to provide a simple template view for the index page.
 
     Attributes:
         template_name (str): The name of the template used to render the view.
-        form_class (Form): The form used for creating the booking.
-        success_url (str): The URL to redirect to after a successful
-        form submission.
     """
-    template_name = 'booking_create.html'
+
+    template_name = 'index.html'
+
+
+class CustomLoginView(LoginView):
+    """
+    A view that handles user authentication and login.
+
+    Inherits from the LoginView provided by AllAuth to provide a customized login view.
+
+    Attributes:
+        template_name (str): The name of the template used to render the view ('login.html').
+    """
+
+    template_name = 'login.html'
+
+
+class BookingCreateView(LoginRequiredMixin, CreateView):
+    """
+    A view that handles the creation of new bookings.
+
+    Inherits from Django's CreateView class and provides the necessary attributes to customize its behavior.
+
+    Attributes:
+        model (Model): The model to use for creating the booking (Booking).
+        form_class (Form): The form used for creating the booking (BookingForm).
+        template_name (str): The name of the template used to render the view.
+        success_url (str): The URL to redirect to after a successful form submission ('booking-list').
+    """
+
+    model = Booking
     form_class = BookingForm
+    template_name = 'booking_create.html'
     success_url = reverse_lazy('booking-list')
 
+    def form_valid(self, form):
+        """
+        Saves the form and sets the user of the booking to the authenticated user.
+        """
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
-class BookingUpdateView(UpdateView):
+
+class BookingUpdateView(LoginRequiredMixin, UpdateView):
     """
     A view that handles the updating of bookings.
 
-    Inherits from Django's UpdateView class and provides the necessary
-     attributes to customize its behavior.
+    Inherits from Django's UpdateView class and provides the necessary attributes to customize its behavior.
 
     Attributes:
-        template_name (str): The name of the template used to render
-         the view ('booking_update.html').
-        form_class (Form): The form used for updating
-         the booking (BookingForm).
-        success_url (str): The URL to redirect to after
-         a successful form submission ('booking-list').
+        model (Model): The model to use for updating the booking (Booking).
+        form_class (Form): The form used for updating the booking (BookingForm).
+        template_name (str): The name of the template used to render the view.
+        success_url (str): The URL to redirect to after a successful form submission ('booking-list').
     """
 
-    template_name = 'booking_update.html'
+    model = Booking
     form_class = BookingForm
+    template_name = 'booking_update.html'
     success_url = reverse_lazy('booking-list')
 
+    def get_queryset(self):
+        """
+        Returns the queryset of bookings for the authenticated user.
+        """
+        return super().get_queryset().filter(user=self.request.user)
 
-class BookingDeleteView(DeleteView):
+
+class BookingDeleteView(LoginRequiredMixin, DeleteView):
     """
     A view that handles the deletion of bookings.
 
-    Inherits from Django's DeleteView class and provides the necessary
-     attributes to customize its behavior.
+    Inherits from Django's DeleteView class and provides the necessary attributes to customize its behavior.
 
     Attributes:
-        template_name (str): The name of the template used to
-         render the view ('booking_delete.html').
         model (Model): The model to use for deleting the booking (Booking).
-        success_url (str): The URL to redirect to
-         after a successful deletion ('booking-list').
+        template_name (str): The name of the template used to render the view.
+        success_url (str): The URL to redirect to after a successful deletion ('booking-list').
     """
 
-    template_name = 'booking_delete.html'
     model = Booking
+    template_name = 'booking_delete.html'
     success_url = reverse_lazy('booking-list')
+
+    def get_queryset(self):
+        """
+        Returns the queryset of bookings for the authenticated user.
+        """
+        return super().get_queryset().filter(user=self.request.user)
